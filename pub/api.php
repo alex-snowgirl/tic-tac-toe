a4eae0be4e557ecc68a4d62582f7035be8f636ae
Oleksandr Andrushchenko <alex.snowgirl@gmail.com>
2019-01-25 22:11:51 +0200

    +log file added

--- pub/api.php
+++ pub/api.php
@@ -5,9 +5,12 @@
  * Date: 12/5/17
  * Time: 7:22 PM
  */
+
 use TIC_TAC_TOE\API;
 
 require '../vendor/autoload.php';
+
+ini_set('error_log', __DIR__ . '/../log.txt');
 
 //simple api call (parse request,execute it and send the response) + all dependencies inside (simple realization)
 new API($_REQUEST);
